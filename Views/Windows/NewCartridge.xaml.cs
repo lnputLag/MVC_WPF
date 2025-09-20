be0c837aa5f188bd5f5cadc216d749f0904e2d1f
lnputLag <kurasovlpack@gmail.com>
2025-09-20 12:05:54 +0300

    Работа над добавлением картриджей

---
+++ Views/Windows/NewCartridge.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MVC_WPF.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для NewCartridge.xaml
+    /// </summary>
+    public partial class NewCartridge : Window
+    {
+        public NewCartridge()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
