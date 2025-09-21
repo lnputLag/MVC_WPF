5deb14afb73b583f3fbb89e36a5f5482677b937b
lnputLag <kurasovlpack@gmail.com>
2025-09-21 17:48:22 +0300

    Подготовка к дальнейшей работе с БД

--- Views/Windows/NewCartridge.xaml.cs
+++ Views/Windows/NewCartridge.xaml.cs
@@ -2,6 +2,7 @@
 using MVC_WPF.Controllers;
 using System;
 using System.Data;
+using MVC_WPF.Data.Database;
 
 namespace MVC_WPF.Views.Windows
 {
@@ -10,15 +11,23 @@ namespace MVC_WPF.Views.Windows
     /// </summary>
     public partial class NewCartridge : Window
     {
+        private DataTable _statuses;
         public NewCartridge()
         {
             InitializeComponent();
+
         }
+
+        
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
             
         }
 
+        private void StatusComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
